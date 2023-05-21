727904c7b37937038070b2d6ca527e63a6d40b28
Tanveer Raza <tanveerraza789@gmail.com>
2023-05-21 20:47:48 +0530

    fix: crc32 algo

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
-use crc::crc32::{Digest, Hasher32};
-use std::hash::Hasher;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 fn calculate_crc32_hex(data: &[u8]) -> String {
-    let mut digest = Digest::new(crc32::IEEE);
-    digest.write(data);
-    format!("{:08x}", digest.sum32())
+    format!("{:08x}", crc32c::crc32c(data))
 }
 
 fn get_sys_time() -> u128 {
@@ -30,17 +26,17 @@ fn get_pid() -> u32 {
 fn mushid() -> String {
     let time = calculate_crc32_hex(&get_sys_time().to_ne_bytes());
     let buff = get_pid().to_string() + "-" + get_mac_address().as_str();
-    let diffrentiator = calculate_crc32_hex(&buff);
-    time.to_string() + diffrentiator.to_str()
+    let diffrentiator = calculate_crc32_hex(buff.as_bytes());
+    time.to_string() + diffrentiator.as_str()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{get_mac_address, mushid};
+    use crate::mushid;
 
     #[test]
     fn it_works() {
         let id = mushid();
-        println!("{:?}", id);
+        println!("{}", id);
     }
 }
